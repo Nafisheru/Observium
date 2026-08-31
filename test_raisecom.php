cac23775168088d24cbfb3f5c3d4ddccd8fa0ed4
Nafisheru <herunapis@gmail.com>
2026-08-31 23:13:26 +0700

    Update test_raisecom.php bootstrap method

--- test_raisecom.php
+++ test_raisecom.php
@@ -1,7 +1,10 @@
 <?php
 chdir('/opt/observium');
-include('includes/sql-config.inc.php');
+$init_modules = array('discovery', 'poller');
+include('includes/init.inc.php');
+if (!function_exists('dbFetchRow')) { include('includes/sql.inc.php'); }
 $device = dbFetchRow("SELECT * FROM devices WHERE hostname = '10.25.0.2'");
 if (!$device) die("Device not found.\n");
+echo "\n[DEBUG] ====== RAISECOM CUSTOM SCRIPT LOADED ======\n";
 include('/opt/observium/includes/discovery/sensors/raisecom-optical-transceiver-mib.inc.php');
 echo "\nDone.\n";
